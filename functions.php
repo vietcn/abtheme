82de0e54dc4972a73fa7c2e8aef3ae30e3b71af2
CMS <support@joomlaman.com>
2017-11-10 17:12:38 +0700

    ok

--- functions.php
+++ functions.php
@@ -92,8 +92,28 @@ add_action( 'after_setup_theme', 'abtheme_content_width', 0 );
 function abtheme_widgets_init()
 {
     register_sidebar( array(
-        'name'          => esc_html__( 'Sidebar', 'abtheme' ),
-        'id'            => 'sidebar-1',
+        'name'          => esc_html__( 'Blog Sidebar', 'abtheme' ),
+        'id'            => 'sidebar',
+        'description'   => esc_html__( 'Add widgets here.', 'abtheme' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => esc_html__( 'Page Sidebar', 'abtheme' ),
+        'id'            => 'sidebar-page',
+        'description'   => esc_html__( 'Add widgets here.', 'abtheme' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => esc_html__( 'Shop Sidebar', 'abtheme' ),
+        'id'            => 'sidebar-shop',
         'description'   => esc_html__( 'Add widgets here.', 'abtheme' ),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget'  => '</section>',
