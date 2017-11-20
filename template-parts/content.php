c1154b0379e17e702923586dfca2900d37a546ff
CMS <support@joomlaman.com>
2017-11-20 13:43:44 +0700

    ok

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,55 +11,65 @@
 
     <div class="entry-featured">
         <?php if(has_post_format('gallery')) : ?>
-
             <?php
-            if (has_post_thumbnail()) {
-                echo '<div class="post-gallery abtheme-post">';
-                the_post_thumbnail('large');
-                echo '</div>';
-            }
+            $light_box = abtheme_get_post_format_value('post-gallery-lightbox','0'); //Value is string ("0" or "1")
+            $gallery_list = explode(',',abtheme_get_post_format_value('post-gallery-images','')); //Value is array of id image list
             ?>
-
         <?php elseif(has_post_format('quote')) : ?>
-
             <?php
-            if (has_post_thumbnail()) {
-                echo '<div class="post-quote abtheme-post">';
-                the_post_thumbnail('large');
-                echo '</div>';
-            }
+            $quote_text = abtheme_get_post_format_value('post-quote-cite',''); //Value is string
+            echo esc_attr($quote_text);
             ?>
-
         <?php elseif(has_post_format('link')) : ?>
-
             <?php
-            if (has_post_thumbnail()) {
-                echo '<div class="post-link abtheme-post">';
-                the_post_thumbnail('large');
-                echo '</div>';
-            }
+            $link_pf = abtheme_get_post_format_value('post-link-url','#');// Value is url
+            echo esc_url($link_pf);
             ?>
-
         <?php elseif(has_post_format('video')) : ?>
-
             <?php
-            if (has_post_thumbnail()) {
-                echo '<div class="post-video abtheme-post">';
-                the_post_thumbnail('large');
-                echo '</div>';
+            $video_url = abtheme_get_post_format_value('post-video-url','#');
+            $video_file = abtheme_get_post_format_value('post-video-file','');
+            $video_html = abtheme_get_post_format_value('post-video-html','');
+            $video = '';
+            if( !empty($video_url) ) {
+                global $wp_embed;
+                echo $wp_embed->run_shortcode( '[embed]' . $video_url . '[/embed]' );
+            }
+            elseif( !empty($video_file) ) {
+                if( strpos('[embed',$video_file) ) {
+                    global $wp_embed;
+                    echo $wp_embed->run_shortcode( $video_file );
+                } else {
+                    echo do_shortcode( $video_file );
+                }
+            }
+            else {
+                if( '' != $video_html ) {
+                    $my_allowed = wp_kses_allowed_html( 'post' );
+
+                    $my_allowed['iframe'] = array(
+                        'align' => true,
+                        'width' => true,
+                        'height' => true,
+                        'frameborder' => true,
+                        'name' => true,
+                        'src' => true,
+                        'id' => true,
+                        'class' => true,
+                        'style' => true,
+                        'scrolling' => true,
+                        'marginwidth' => true,
+                        'marginheight' => true,
+                    );
+                    echo wp_kses( $video_html, $my_allowed );
+                }
             }
             ?>
 
         <?php elseif(has_post_format('audio')) : ?>
-
             <?php
-            if (has_post_thumbnail()) {
-                echo '<div class="post-audio abtheme-post">';
-                the_post_thumbnail('large');
-                echo '</div>';
-            }
+            $audio_url = abtheme_get_post_format_value('post-audio-url','#');
             ?>
-
         <?php else : ?>
 
             <?php
