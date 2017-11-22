f3bb90e8edb7b6fa2b2dff0610a4316792b120af
KP <kenzy194@gmail.com>
2017-11-22 16:35:38 +0700

    Kp Update header

--- template-parts/header-menu.php
+++ template-parts/header-menu.php
@@ -5,20 +5,20 @@
 
 if ( has_nav_menu( 'primary' ) )
 {
-    wp_nav_menu( array(
-        'theme_location' => 'primary',
-        'menu_id'        => 'mastmenu',
-        'menu_class'     => 'primary-menu',
-        'container'      => ''
-    ) );
-
 //    wp_nav_menu( array(
 //        'theme_location' => 'primary',
-//        'container'  => '',
-//        'menu_id'    => 'mastmenu',
-//        'menu_class' => 'primary-menu',
-//        'walker'         => class_exists( 'EFramework_Mega_Menu_Walker' ) ? new EFramework_Mega_Menu_Walker : '',
+//        'menu_id'        => 'mastmenu',
+//        'menu_class'     => 'primary-menu',
+//        'container'      => ''
 //    ) );
+
+    wp_nav_menu( array(
+        'theme_location' => 'primary',
+        'container'  => '',
+        'menu_id'    => 'mastmenu',
+        'menu_class' => 'primary-menu',
+        'walker'         => class_exists( 'EFramework_Mega_Menu_Walker' ) ? new EFramework_Mega_Menu_Walker : '',
+    ) );
 }
 else
 {
