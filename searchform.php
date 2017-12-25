f829cd353f0a0eadb0bfeb0a69dae45d93aa7b01
CMS <support@joomlaman.com>
2017-12-25 16:21:17 +0700

    ok

--- searchform.php
+++ searchform.php
@@ -5,8 +5,8 @@
 ?>
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url( '/' )); ?>">
-	<div class="searchform-wrap">
-        <input type="text" placeholder="<?php esc_html_e('Search on the site', 'abtheme'); ?>" name="s" class="search-field" />
-        <button type="submit" class="btn btn-primary search-submit"><i class="fa fa-search"></i></button>
+    <div class="searchform-wrap">
+        <input type="text" placeholder="<?php esc_html_e('Search on the site', 'abtheme'); ?>" name="s" class="search-field pull-left" />
+        <button type="submit" class="btn btn-primary search-submit pull-right"><i class="fa fa-search"></i></button>
     </div>
 </form>
